633025aa161428ffa342a61b201d083d8c95b529
sourav jaiswal <souravjaiswal2000@gmail.com>
2022-01-22 16:15:25 +0530

    Big Update

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ const port = process.env.PORT || 80;
 const Api_KEY = process.env.Api_KEY
 
 
+
 //VariableNeedsToLoadEveryTimePageIsRefreshed
-var titleID = "";
-var movie = "";
-var movie_placeholder = "";
-var lives = 9;
+const map = new Map();
 
 //static
 app = express();
@@ -23,86 +21,119 @@ app.use(express.json({ limit: '1mb' }));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-
-app.get("/won", (req, res) => {
-    if (!won()) {
-        res.render("Error404.pug");
-    } else {
-        const params = { titleID: titleID, Api_KEY: Api_KEY };
-        res.render("win.pug", params);
-    }
-});
-
-app.get("/lost", (req, res) => {
-    if (lives > 0) {
-        res.render("Error404.pug");
-    } else {
-        const params = { titleID: titleID, Api_KEY: Api_KEY };
-        res.render("lost.pug", params);
-
-    }
-})
-app.get("/start", (req, res) => {
-    lives = 9;
-    var ret_value = get_random_movie();
-    movie = ret_value[0].toUpperCase();
-    titleID = ret_value[1];
-    // console.log(movie + ";" + titleID);
-    movie_placeholder = movie.replace(/ /g, "/").replace(/[A-Za-z]/g, "_").split("");
-    data = {
-        movie_placeholder: movie_placeholder
-    };
-    res.json(data);
-})
-
-app.post("/api", (req, res) => {
-    // console.log(req.body);
-    flag = fill_characters(req.body.character);
-    win = won();
-    data = {
-        character: req.body.character,
-        movie_placeholder: movie_placeholder,
-        correct: flag,
-        win: win
-    };
-    res.json(data);
-
-})
-
-app.get("/depleteLive", (req, res) => {
-    var life = 0;
-    if (lives > 0)
-        life = --lives;
-
-    data = {
-        life: life
-    };
-    res.json(data);
-})
-
+//Setting up listenting Port
 app.listen(port, () => {
     console.log("Server Started on port");
 })
 
 
+//ENDPOINT 
 
-function fill_characters(character) {
-    character = character.toUpperCase();
-    flag = false;
-    for (var i = 0; i < movie.length; i++) {
-        if (movie_placeholder[i] == '/') {
-            continue;
-        } else {
-            if (movie.charAt(i) == character) {
-                flag = true;
-                movie_placeholder[i] = character;
-            }
-        }
+//start
+app.get("/start", (req, res) => {
+
+
+    var ret_value = get_random_movie();
+    var random_id;
+
+    //Assign the incoming Request with random id
+    do {
+        random_id = Math.floor(Math.random() * 1000);
+    } while (map.has(random_id));
+
+    movie = ret_value[0].toUpperCase();
+    titleID = ret_value[1];
+    movie_placeholder = movie.replace(/ /g, "/").replace(/[A-Za-z]/g, "_").split("");
+
+
+    //Create a game json for that id
+    map.set(random_id, {
+        lives: 9,
+        movie: movie,
+        titleID: titleID,
+        movie_placeholder: movie_placeholder,
+        won: false
+    });
+
+    res.json({
+        id: random_id,
+        movie_placeholder: movie_placeholder
+    });
+})
+
+
+//API
+app.post("/api", (req, res) => {
+    var data = map.get(req.body.id)
+
+    data = fill_characters(req.body.character, data);
+    flag = data.flag;
+
+    data = won(data.json);
+
+    `   `
+    map.set(req.body.id, data);
+    const ret = {
+        movie_placeholder: data.movie_placeholder,
+        correct: flag,
+        win: data.won
+    };
+
+    res.json(ret);
+})
+
+
+app.post("/depleteLive", (req, res) => {
+
+    const data = map.get(req.body.id);
+
+    var life = 0;
+    if (data.lives > 0)
+        life = --data.lives;
+
+    map.set(req.body.id, data);
+
+    res.json({
+        life: life
+    });
+})
+
+
+app.get("/won/:id", (req, res) => {
+
+    const data = map.get(parseInt(req.params.id));
+    if (!data) {
+        res.render("Error404.pug");
+    } else {
+        dlete(req.params.id);
+        // map.delete(req.params.id);
+        const params = { titleID: data.titleID, Api_KEY: Api_KEY };
+        res.render("win.pug", params);
     }
-    return flag;
+});
 
-}
+app.get("/lost/:id", (req, res) => {
 
+    const data = map.get(parseInt(req.params.id));
+    if (!data) {
+        res.render("Error404.pug");
+    } else {
+        dlete(req.params.id);
+        // map.delete(req.params.id);
+        const params = { titleID: data.titleID, Api_KEY: Api_KEY };
+        res.render("lost.pug", params);
+    }
+
+
+})
+
+app.get("/userLeft/:id", (req, res) => {
+    dlete(req.params.id);
+})
+
+
+
+//UTILITY FUNCTIONS
 function get_random_movie() {
     const list = fs.readFileSync("./views/movie_list.txt", { encoding: 'utf8' }).split(/\r\n|\n/);
     var ran = Math.floor(Math.random() * list.length);
@@ -110,9 +141,39 @@ function get_random_movie() {
 
 }
 
-function won() {
-    for (character of movie_placeholder)
-        if (character == "_")
-            return false;
-    return true;
+function fill_characters(character, data) {
+    flag = false;
+
+    for (var i = 0; i < data.movie.length; i++) {
+        if (data.movie_placeholder[i] != '/' && data.movie.charAt(i) == character) {
+            flag = true;
+            data.movie_placeholder[i] = character;
+        }
+    }
+
+    return { flag: flag, json: data };
+
 }
+
+function dlete(id) {
+    map.delete(parseInt(id));
+}
+
+function won(data) {
+    data.won = true;
+
+    for (character of data.movie_placeholder)
+        if (character == "_") {
+            data.won = false;
+            break;
+        }
+    return data;
+}
+
+setInterval(function() {
+
+    var date = new Date(); // Create a Date object to find out what time it is
+    if (date.getHours() === 12 && date.getMinutes() === 00) { // Check the time
+        map.clear();
+    }
+}, 50000);
--- static/JS/script.js
+++ static/JS/script.js
@@ -1,8 +1,11 @@
+var id = null;
+var won_or_lost = false;
+
 function render_character(character) {
     document.getElementsByClassName("dynamicText")[0].innerHTML = character.toUpperCase();
 }
 
-function render_message(character, correct) {
+async function render_message(character, correct) {
     var happy = [9757, 9889, 9996, 10004, 128526, 127774, 128525, 128584];
     var sad = [128580, 128577, 128575, 128561, 128560, 128557, 128555, 128545];
     var random = Math.floor(Math.random() * 8);
@@ -16,8 +19,16 @@ function render_message(character, correct) {
 }
 
 async function depleteLive() {
-    const response = await fetch("/depleteLive");
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: id })
+    };
+    const response = await fetch("/depleteLive", options);
     const json = await response.json();
+
     if (json.life <= 0) {
         lost();
     }
@@ -27,6 +38,7 @@ async function depleteLive() {
 }
 
 function turn_keyboard_off(message, url) {
+    won_or_lost = true;
     document.getElementById("messages").innerHTML = message;
     document.querySelectorAll(".key").forEach(element => {
         element.classList.add("off");
@@ -34,7 +46,7 @@ function turn_keyboard_off(message, url) {
         element.onclick = null;
     });
     setInterval(() => {
-        window.location = url;
+        window.location = url + "/" + id;
     }, 1500);
 }
 
@@ -46,26 +58,22 @@ function game_won() {
     turn_keyboard_off("You Won!", "won")
 }
 
-function stringjoin(movie_placeholder) {
-    var return_string = "";
-    for (character of movie_placeholder) {
-        if (character == "_") {
-            return_string += "__ ";
-        } else if (character == "/")
-            return_string += "&nbsp&nbsp";
-        else {
-            return_string += character + " ";
-        }
+const userLeft = (e) => {
+    if (!won_or_lost) {
+        fetch("userLeft/" + id);
+        e.preventDefault();
+        e.returnValue = 'Hey';
     }
-    return return_string.trim();
 }
 
 function keyboardEvent(event) {
-    var x = event.key;
+    var x = event.key.toUpperCase();
+
     charCode = x.charCodeAt(0);
-    if (x.length == 1 && (charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
-        document.getElementById(x.toUpperCase()).focus();
-        func(x.toUpperCase());
+
+    if (x.length == 1 && (charCode >= 65 && charCode <= 90)) {
+        // document.getElementById(x).focus();
+        func(x);
     }
 }
 
@@ -74,14 +82,19 @@ async function func(id_name) {
     var element = document.getElementById(id_name);
 
     if (!element.onclick) {
+        alert("You Have Already selected: " + id_name);
         return "";
     }
+
     element.classList.add("off");
     element.classList.remove("key");
     element.onclick = null;
     render_character(id_name);
 
-    const data = { "character": id_name };
+    const data = {
+        id: id,
+        character: id_name
+    };
 
     const options = {
         method: "POST",
@@ -96,21 +109,14 @@ async function func(id_name) {
 
 
     render_movie(json);
-    render_message(json.character, json.correct);
+    render_message(id_name, json.correct);
     if (json.win)
         game_won();
 
 }
 
 
-async function start() {
-    const response = await fetch("/start");
-    const recived_data = await response.json();
-    const movie_placeholder = stringjoin(recived_data.movie_placeholder);
-    document.querySelector("#gameArea>span").innerHTML = movie_placeholder;
-}
-
-function render_movie(data) {
+async function render_movie(data) {
     if (data.correct) {
         const movie_placeholder = stringjoin(data.movie_placeholder);
         document.querySelector("#gameArea>span").innerHTML = movie_placeholder;
@@ -119,4 +125,31 @@ function render_movie(data) {
         depleteLive();
     }
 }
+
+
+
+function stringjoin(movie_placeholder) {
+    var return_string = "";
+    for (let character of movie_placeholder) {
+        if (character == "_") {
+            return_string += "__ ";
+        } else if (character == "/")
+            return_string += "&nbsp&nbsp";
+        else {
+            return_string += character + " ";
+        }
+    }
+    return return_string.trim();
+}
+
+
+async function start() {
+    const response = await fetch("/start");
+    const recived_data = await response.json();
+    id = recived_data.id;
+    // console.log(id);
+    document.querySelector("#gameArea>span").innerHTML = stringjoin(recived_data.movie_placeholder);
+}
+
+addEventListener("beforeunload", userLeft, { capture: true });
 start();
